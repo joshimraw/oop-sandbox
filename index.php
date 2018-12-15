30666b927161d80e2e1a57f3d620115a22317847
joshimraw <joshim7m@gmail.com>
2018-12-15 22:30:38 +0600

    Magic mathod --get and --set

--- index.php
+++ index.php
@@ -1,24 +1,44 @@
 <?php 
 
 class Users{
-	public $name;
-	public $age;
+	private $name;
+	private $age;
 
 	public function __construct($name, $age){
-		echo 'Class ' .__CLASS__. ' is instantiated <br>';
 		$this->name = $name;
 		$this->age = $age;
 	}
 
+	// public function getName(){
+	// 	return $this->name;
+	// }
 
-	public function __destruct(){
-		echo 'Class ' .__CLASS__. 'has destroyed';
+	// public function setName($name){
+	// 	$this->name = $name;
+	// }
+
+	public function __get($property){
+		if(property_exists($this, $property)){
+			return $this->$property;
+		}
 	}
+
+	public function __set($property, $value){
+		if(property_exists($this, $property)){
+			$this->$property = $value;
+		}
+		return $this;
+	}
+
 }
 
-
 $user = new Users('john', 25);
-echo $user->name . ' is ' .$user->age .' years old <br>';
 
+//echo $user->name; // cant access private name propoerty
+//echo $user->name = 'jeff'; // cant access private name propoerty
 
+// $user->setName('Jeff');
+// echo $user->getName();
+$user->__set('name', 'Jeff');
+echo $user->__get('name');
 
