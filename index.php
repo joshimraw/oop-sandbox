d0daf3eb5ce327ccaef6dbf444e72a8375686b39
joshimraw <joshim7m@gmail.com>
2018-12-15 22:46:55 +0600

    class inheritance

--- index.php
+++ index.php
@@ -1,44 +1,33 @@
 <?php 
 
 class Users{
-	private $name;
-	private $age;
+	protected $name;
+	protected $age;
 
 	public function __construct($name, $age){
 		$this->name = $name;
 		$this->age = $age;
 	}
 
-	// public function getName(){
-	// 	return $this->name;
-	// }
+}
 
-	// public function setName($name){
-	// 	$this->name = $name;
-	// }
+class Customer extends Users{
+	private $balance;
 
-	public function __get($property){
-		if(property_exists($this, $property)){
-			return $this->$property;
-		}
+	public function __construct($name, $age, $balance){
+		parent::__construct($name, $age);
+		$this->balance = $balance;
 	}
 
-	public function __set($property, $value){
-		if(property_exists($this, $property)){
-			$this->$property = $value;
-		}
-		return $this;
+	public function pay($amount){
+		return $this->name . ' paid $ ' .$amount;
+	}
+
+	public function getBalance(){
+		return $this->balance;
 	}
 
 }
 
-$user = new Users('john', 25);
-
-//echo $user->name; // cant access private name propoerty
-//echo $user->name = 'jeff'; // cant access private name propoerty
-
-// $user->setName('Jeff');
-// echo $user->getName();
-$user->__set('name', 'Jeff');
-echo $user->__get('name');
-
+$customer1 = new Customer('John', 35, 255);
+echo $customer1->getBalance();
